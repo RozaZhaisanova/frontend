b7b87807760927c789f90cf30e343d3e6bcbf6ac
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-03-19 11:43:22 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import AddClient from "./components/add-client.component";
-import Client from "./components/client.component";
-import ClientsList from "./components/clients-list.component";
+
+import PersonList from './components/PersonList';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/clients" className="navbar-brand">
-            Netcracker
-          </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/clients"} className="nav-link">
-                Clients
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/clients"]} component={ClientsList} />
-            <Route exact path="/add" component={AddClient} />
-            <Route path="/clients/:id" component={Client} />
-          </Switch>
-        </div>
+      <PersonList/>
       </div>
     );
   }
